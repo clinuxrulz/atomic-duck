b62bb42cc09a57447fbc01302061df0dbd550fde
Clinton Selke <clinuxrulz@gmail.com>
2025-08-14 05:01:13 +1000

    wip

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export type Signal<A> = [ Accessor<A>, Setter<A>, ];
 
 interface Node {
     age: number,
-    nexts: (() => void)[],
+    update: () => void,
 };
 
 let time = 0;
@@ -15,11 +15,7 @@ function flush() {
     let oldCursors = cursors;
     cursors = new Set<Node>();
     for (let cursor of oldCursors) {
-        let oldNexts = cursor.nexts;
-        cursor.nexts = [];
-        for (let next of oldNexts) {
-            next();
-        }
+        cursor.update();
     }
 }
 
@@ -42,21 +38,22 @@ export function createMemo<A>(k: (ret: (a: A) => void) => void): Accessor<A> {
     let hasValue = false;
     let node: Node = {
         age: time++,
-        nexts: [],
+        update: () => {},
     };
     throw new Error("TODO");
 }
 
 export function createSignal<A>(a: A): Signal<A> {
     let value = a;
+    let nexts: (() => void)[] = [];
     let node: Node = {
         age: time++,
-        nexts: [],
+        update: () => {},
     };
     cursors.add(node);
     return [
         (ret) => {
-            node.nexts.push(() => ret(value));
+            nexts.push(() => ret(value));
         },
         (x) => {
             value = x;
